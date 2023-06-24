aa946849ab8eb6c027c8f873b31717bf14d03161
Hendrik Buschbaum <hendrik.buschbaum@rwth-aachen.de>
2023-06-24 14:08:09 +0200

    final changes

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import socket, signal, struct, tkinter as tk, threading, sys, numpy as np
 BUFFER_SIZE=1024
 WIDTH=1000
 HEIGHT=500
-A=100
+A=100 #side length of middle square
 
 run = True
 
@@ -12,36 +12,36 @@ def signal_handler(x,y):
     run = False
     sys.exit(0)
 
-def reset(canvas: tk.Canvas, id):
-    canvas.moveto(id, WIDTH/2-A/2, HEIGHT/2-A/2)
-
 signal.signal(signal.SIGINT, signal_handler)
 
-ip = "192.168.2.4" #input("IP: ")
+print("Hello, please enter the IP of the Winkly Server :)")
+ip = input("IP: ")
 
 window = tk.Tk()
 window.geometry("%dx%d" % (WIDTH,HEIGHT+50))
 
 canvas = tk.Canvas(window, width=WIDTH, height=HEIGHT)
 canvas.pack()
+#create figures
 id_rect = canvas.create_rectangle(WIDTH/2-A/2,HEIGHT/2-A/2,WIDTH/2+A/2,HEIGHT/2+A/2, fill="red")
 id_line = canvas.create_line(WIDTH/2,HEIGHT/2,WIDTH/2,HEIGHT,fill="yellow",width=3)
+id_arc = canvas.create_arc((WIDTH-HEIGHT)/2, 0, (WIDTH+HEIGHT)/2, HEIGHT, start=180, extent=180)
 
-
-#button = tk.Button(window, text="Reset", command=lambda: reset(canvas, id))
-#button.pack()
-
-def do_smth(canvas, id, ip):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, 6969))
+#get data from server and refresh canvas in background
+def do_smth(canvas: tk.Canvas, id, ip):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, 6969))
+    except:
+        canvas.create_text(100,100,text="No connection to server!")
+        return
     while run:
         data = b''
         while len(data) < 4:
             data += s.recv(BUFFER_SIZE)
-        num = struct.unpack("!i",data[:4])[0] / 1000
+        num = -struct.unpack("!i",data[:4])[0] / 1000
         print(num)
         canvas.coords(id, WIDTH/2,HEIGHT/2,WIDTH/2+np.sin(np.deg2rad(num))*HEIGHT/2, HEIGHT/2+np.cos(np.deg2rad(num))*HEIGHT/2)
-        #canvas.move(id, num,0)
 
 threading.Thread(target=do_smth, args=(canvas, id_line, ip,)).start()
 window.mainloop()
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+#authors: DieDosenTomaten - Jannik, Raphael, Hendrik 24.06.2023
+
 import sounddevice as sd
 import numpy as np
 import queue, threading
@@ -10,17 +12,20 @@ device = 'snd_rpi_i2s_card'
 blocksize = 1000
 
 secondsPerStep = 1/samplerate 
-buffer  = np.zeros(shape=(blocksize + blocksize//10,2))
+buffer  = np.zeros(shape=(blocksize + blocksize//10,2)) #to have a bit of sliding window we at a tenth of the blocksize to the buffersize
 change = False
 q = queue.Queue()
 
 parse = argparse.ArgumentParser()
 parse.add_argument("-s", action="store_true")
+parse.add_argument("--silent", action="store_true")
 args = parse.parse_args()
 
 def get_WiNkEl(diff):
+    #calculates the angle from the calculated time differential
     return np.rad2deg(np.arcsin((diff * secondsPerStep  * 343.2 )/ 0.044))
 
+#server function to send data to client
 def server(conn: socket.socket):
     while 1:
         val = struct.pack("!i", q.get())
@@ -30,32 +35,31 @@ def server(conn: socket.socket):
             break
     conn.close()
 
-def m_fun(tpl):
-    tpl = (abs(tpl[0]), abs(tpl[1]))
-
 def processBuffer():
     global buffer
 
-    f0, _ = find_peaks([i[0] for i in buffer], height=0.012, distance=20)
+    f0, _ = find_peaks([i[0] for i in buffer], height=0.012, distance=20) #only process further if there are peaks over 0.012
 
     if len(f0) == 0 :
         return
 
-    d0 = np.array([buffer[i][0] * buffer[i][0] for i in range(len(buffer))])
-    d1 = np.array([buffer[i][1] * buffer[i][1] for i in range(len(buffer))])
+    d0 = np.array([buffer[i][0] * buffer[i][0] for i in range(len(buffer))]) #sqaure all Elements in Buffer
+    d1 = np.array([buffer[i][1] * buffer[i][1] for i in range(len(buffer))]) #so that small values become smaller and big values become bigger
 
-    data0 = d0 - np.mean(d0)
+    data0 = d0 - np.mean(d0) #normalize arrays
     data1 = d1 - np.mean(d1)
 
-    corr = signal.correlate(data0, data1, "full", "fft").argmax()#-(len(data0)-1)
-    lags = signal.correlation_lags(data0.size, data1.size, "full")
-    lag = lags[corr]
-    if abs(lag) > 7:
+    corr = signal.correlate(data0, data1, "full", "fft").argmax() #correlation between left and right channel
+    lags = signal.correlation_lags(data0.size, data1.size, "full") 
+    lag = lags[corr] #get lag (delay) from lag function
+    if abs(lag) > 6: #all lags above 6 are not processible with our microphones
         return
-    winkel =(get_WiNkEl(lag))
-    print(winkel)
+    winkel = get_WiNkEl(lag)
+    if not args.silent:
+        print(winkel)
+
     if args.s and not np.isnan(winkel):    
-        q.put(int(winkel*1000))
+        q.put(int(winkel*1000)) #send data to client
 
 
 def callback(indata, frames, time, status):
@@ -64,12 +68,13 @@ def callback(indata, frames, time, status):
     global change
     if status:
         print(status)
-    buffer = np.roll(buffer,blocksize//10,axis=0)
-    buffer[blocksize//10:] = abs(indata)
+    buffer = np.roll(buffer,blocksize//10,axis=0) #slide window
+    buffer[blocksize//10:] = abs(indata) #add new data
 
     change = True
 
 if args.s:
+    #setup server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(("0.0.0.0", 6969))
     s.listen(1)
